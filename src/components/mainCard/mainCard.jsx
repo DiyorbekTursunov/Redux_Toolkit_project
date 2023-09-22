9fa16479f4c740faea13bec603a7cc4e0cac2d40
Diyorbek <tursunovdiyorbek570@gmail.com>
2023-09-22 10:13:25 +0500

    change catigory

--- src/components/mainCard/mainCard.jsx
+++ src/components/mainCard/mainCard.jsx
@@ -2,14 +2,16 @@ import MainCardDetail from './mainCardDetail';
 import DataCatigory from '../catigory/dataCatigory';
 
 const MainCard = () => {
- 
+
 
   return (
     <>
       <div className='w-full  mx-auto pb-[170px]'>
       </div>
-      <div className='max-w-[1110px] flex  mx-auto px-5 pb-[100px]'>
-        <DataCatigory />
+      <div className='max-w-[1110px] flex gap-3 mx-auto px-5 pb-[100px]'>
+        <div className='bg-[#135158] py-5'>
+          <DataCatigory />
+        </div>
         <MainCardDetail />
       </div>
     </>
