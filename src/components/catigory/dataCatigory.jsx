f6d22afc4a5cfd92f934a04f29c429d0b0ea2539
Diyorbek <tursunovdiyorbek570@gmail.com>
2023-09-22 10:09:11 +0500

    change catigory

--- src/components/catigory/dataCatigory.jsx
+++ src/components/catigory/dataCatigory.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getCatigory } from '../../redux/CartigorySlice'
 
 const DataCatigory = () => {
     const [catigors, setcatigors] = useState(null)
+    const { catigory } = useSelector(state => state.catigory)
     const dispatch = useDispatch()
     const getCatigoryHandel = () => {
         try {
@@ -23,7 +24,7 @@ const DataCatigory = () => {
   return (
     <div className='text-white flex flex-col lg:gap-[20px] mb-3 md:gap-[5px] items-start pb-2 mx-10 px-10'>
         {catigors && catigors.map((item , i) => (
-            <button onClick={() => dispatch(getCatigory(item))} className='py-[8px] whitespace-nowrap px-[12px] rounded-[32px] border-[2px] border-[#303030] hover:opacity-70 cursor-pointer' key={i}><span>{item}</span></button>
+            <button onClick={() => dispatch(getCatigory(item))} className={`py-[8px] whitespace-nowrap px-[12px] rounded-[32px] border-[2px] border-[#303030] hover:opacity-70 cursor-pointer ${item == catigory ? '' : 'bg-slate-800'}`} key={i}><span>{item}</span></button>
         ))}
     </div>
   )
